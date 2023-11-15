4a58dbed23c7eca26e1bd7dccd39c019f820f592
dinaemadroshdy <dinaemadroshdy@gmail.com>
2023-11-15 15:33:18 -0800

    b

--- main.c
+++ main.c
@@ -4,14 +4,19 @@
  * main - Entry point
  *
  * Description: Display prompt
+ * @argv: An array of strings containing the command-line arguments.
  * Return: 0
  */
 
-int main(void)(int ac; char *argv)
+int main(int ac; char *argv)
 {
-char *prompt = "(hdshell) $";
-
+char *prompt = "(hdshell) $ ";
+(void)ac;
+(void)argv;
+char *sentptr;
+size_t n = 0;
 printf("%s", prompt);
-
+getline(&sentptr, &n, stdin);
+	free(sentptr);
 	return (0);
 }
