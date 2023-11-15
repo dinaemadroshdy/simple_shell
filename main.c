16fd7b0db21f1cc80b234ede2333edf9b536e682
dinaemadroshdy <dinaemadroshdy@gmail.com>
2023-11-15 04:35:53 -0800

    A

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Display prompt
+ * Return: 0
+ */
+
+int main(void)(int ac; char *argv)
+{
+char *prompt = "(hdshell) $";
+
+printf("%s", prompt);
+
+	return (0);
+}
